ac02f4cac47fcf0c92cc841f93de2442e5974b51
julie1013 <julie1380@verizon.net>
2016-10-14 14:51:41 -0400

    Add uploads contorller
    Use middleware for multer memory config.
    Do not upload or save to datastore yet.
    
    Next we will upload to S3 from our controller and to the datastore.

--- config/routes.js
+++ config/routes.js
@@ -17,5 +17,10 @@ module.exports = require('lib/wiring/routes')
 .patch('/change-password/:id', 'users#changepw')
 .resources('users', { only: ['index', 'show'] })
 
+//custom routes
+.post('./uploads', 'uploads#create');
+
+//equivalent to:
+// .resources('/uploads', { only: ['create']});
+
 // all routes created
-;
