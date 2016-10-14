bece902a4e7b9e4e9a2e764079e65b7c099c1f21
julie1013 <julie1380@verizon.net>
2016-10-14 11:45:36 -0400

    Name file uploads using crypto module.
    Define wrapper functions for naming the file and the directory.
    TIL: Be really careful with Promises.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,9 +1,13 @@
-#!/usr/bin/env node
-
 'use strict';
 
+// this has to come before anything else
+require('dotenv').config();
+
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
+const AWS = require('aws-sdk');
 
 const filename = process.argv[2] || '';
 
@@ -19,8 +23,8 @@ const readFile = (filename) => {
   });
 };
 
-//return a default object in the case that fileType is given an unsupported
-//file type to read
+// return a default object in the case that fileType is given an unsupported
+// filetype to read
 const mimeType = (data) => {
   return Object.assign({
     ext: 'bin',
@@ -34,33 +38,74 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-const upload = (file) => {
-  const options = {
-    //get the bucket name from your AWS S3 console
-    Bucket: 'julie1013',
-    //attach the fileBuffer as a stream to send to Amazon
-    Body: file.data,
-    //allow anyone to access the URL of the uploaded file
-    ACL: 'public-read',
-    //tell S3 what the mime-type is
-    ContentType: file.mime,
-    //pick a filename for S3 to use for the upload
-    Key: `test/test.${file.ext}`
-  };
-  //don't actually upload yet; just pass the data down the Promise chain
-  return Promise.resolve(options);
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
 };
 
-const logMessage = (upload) => {
-  //get rid of the stream for now, so I can log the rest of my options in the
-  //terminal without seeing the stream
-  delete upload.Body;
-  //turn the pojo into a string so I can see it in the console.
-  console.log(`The upload options are ${JSON.stringify(upload)}`);
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  }
+});
+
+const upload = (file) => {
+  const options = {
+    // get the bucket name from your AWS S3 console
+    Bucket: 'julie1013',
+    // attach the fileBuffer as a stream to send to S3
+    Body: file.data,
+    // allow anyone to access the URL of the uploaded file
+    ACL: 'public-read',
+    // tell S3 what the mime-type is
+    ContentType: file.mime,
+    // pick a filename for S3 to use for the upload
+    Key: `${file.dir}/${file.name}.${file.ext}`
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.upload(options, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(data);
+    });
+  });
+};
+
+const logMessage = (response) => {
+  // turn the pojo into a string so I can see it on the console
+  console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
-.catch(console.error);
+// .then(console.log)
+.catch(console.error)
+;
